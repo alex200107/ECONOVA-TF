1d1dffabf1b4ee7e29fcc8dd93d8e347d837d77e
ALEXANDER ZAMBRANO <67656687+alex200107@users.noreply.github.com>
2022-06-30 10:30:55 -0500

    Update ITypeSuscriptionRepository.java

--- EconovaSpring/src/main/java/pe/edu/upc/demo/repositories/ITypeSuscriptionRepository.java
+++ EconovaSpring/src/main/java/pe/edu/upc/demo/repositories/ITypeSuscriptionRepository.java
@@ -1,13 +1,9 @@
 package pe.edu.upc.demo.repositories;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import pe.edu.upc.demo.entities.TypeSuscription;
-
 @Repository
 public interface ITypeSuscriptionRepository extends JpaRepository<TypeSuscription, Integer> {
 
